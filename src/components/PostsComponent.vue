64722b517ce79af1f471b20a63fc0dfcc4fa83b7
rechim25 <radu.echim.19@ucl.ac.uk>
2021-01-21 17:10:23 +0200

    feat: Make search area layout mobile responsive

--- src/components/PostsComponent.vue
+++ src/components/PostsComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container fluid>
-    <b-container fluid="sm" class="search-container">
+    <b-container fluid="sm" class="search-container mx-auto">
       <b-row align-h="center" class="mt-5 mb-3">
         <b-col class="d-flex">
           <label for="subreddit-search" class="header-text mx-auto">{{
@@ -11,25 +11,27 @@
       <b-row
         id="subreddit-search"
         align-h="center"
-        class="mb-3 flex-nowrap search-first-row"
+        class="mb-3 d-flex search-first-row"
       >
-        <b-col md="4" class="self-align-center">
-          <b-form-input
-            class="search-input-group search-text-area"
-            placeholder="Enter a subreddit"
-            v-model="inputSubreddit"
-            @keyup.enter="getTopPostsFromSubreddit"
-          ></b-form-input>
-        </b-col>
-        <b-col md="2" class="self-align-center d-flex">
-          <b-form-select
-            class="search-input-group timeframe-select mx-auto"
-            v-model="selectedTimeframe"
-            :options="timeframeOptions"
-            @change="getTopPostsFromSubreddit"
-          >
-          </b-form-select>
-        </b-col>
+        <div class="d-flex flex-nowrap">
+          <b-col md="4" class="self-align-center mx-auto">
+            <b-form-input
+              class="search-input-group search-text-area"
+              placeholder="Enter a subreddit"
+              v-model="inputSubreddit"
+              @keyup.enter="getTopPostsFromSubreddit"
+            ></b-form-input>
+          </b-col>
+          <b-col md="2" class="self-align-center d-flex mx-auto">
+            <b-form-select
+              class="search-input-group timeframe-select mx-auto"
+              v-model="selectedTimeframe"
+              :options="timeframeOptions"
+              @change="getTopPostsFromSubreddit"
+            >
+            </b-form-select>
+          </b-col>
+        </div>
         <b-col md="2" class="self-align-center d-flex">
           <b-button
             class="search-btn search-input-group shadow mx-auto"
@@ -39,7 +41,7 @@
         </b-col>
       </b-row>
     </b-container>
-    <b-container fluid="sm" class="mt-4">
+    <b-container fluid="sm" class="mt-1">
       <b-row align-h="center">
         <b-col class="post-listing-col d-flex">
           <div class="mx-auto">
@@ -70,7 +72,7 @@ import { fetchTopPostsFromSubreddit } from "../apiService.js";
 import PostCard from "./PostCard.vue";
 import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 
-const DEFAULT_LIMIT = 25;
+const DEFAULT_LIMIT = 50;
 const DEFAULT_ERROR = "Something went wrong...";
 const timeframeQueryMap = {
   Now: "now",
@@ -173,11 +175,12 @@ export default {
 
 .timeframe-select {
   max-width: 100px;
+  min-width: 85px;
 }
 
 .search-input-group {
   box-shadow: 0px 1px 4px 0px rgb(224, 224, 224);
-  margin: 5px auto;
+  margin: 10px auto;
 }
 
 .search-input-group:focus {
@@ -189,6 +192,13 @@ export default {
   background-color: var(--blue-dark) !important;
   box-shadow: 4px 5px 5px 5px solid rgb(75, 75, 75) !important;
   border-color: var(--purple-light) !important;
+  margin: 30px auto;
+}
+
+@media only screen and (min-width: 768px) {
+  .search-btn {
+    margin: 10px auto;
+  }
 }
 
 .search-btn:hover {
